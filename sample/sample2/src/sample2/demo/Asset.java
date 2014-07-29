77d6f9087ce6d9537a9d9cb4d9c798ed1e8ec7d7
kubota <kubota@tets.jp>
2014-07-29 18:49:59 +0900

    xsx

--- sample/sample2/src/sample2/demo/Asset.java
+++ sample/sample2/src/sample2/demo/Asset.java
@@ -1,13 +1,13 @@
 package sample2.demo;
 
 public abstract class Asset {
-	
+
 	private String name;
 	private int price;
-	
+
 	public String getName(){return this.name;}
 	public int getPrince(){return this.price;}
-	
+
 		public Asset(String name,int price){
 		this.name = name;
 		this.price = price;
